0ad5cad6c54e321dd908e4056df6a5aeb2ba82e4
zyhds <zyhdsbhhhhh@gmail.com>
2026-09-18 16:38:48 +0800

    Bump host updater version to 0.2.1

--- MintDeskHost/src/HostApp.cpp
+++ MintDeskHost/src/HostApp.cpp
@@ -26,7 +26,7 @@ constexpr int kUpdateButton = 1006;
 constexpr UINT_PTR kTimerId = 1;
 constexpr UINT kStatusMessage = WM_APP + 1;
 constexpr UINT kUpdateFinishedMessage = WM_APP + 2;
-constexpr wchar_t kCurrentVersion[] = L"0.2.0";
+constexpr wchar_t kCurrentVersion[] = L"0.2.1";
 constexpr wchar_t kManifestUrl[] = L"https://raw.githubusercontent.com/mikasasukida/MintDesk/main/release/latest.json";
 
 HWND g_status = nullptr;
